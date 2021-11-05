1835b6532bd2d03e1d6a34e52090456a82978c66
Jordan <jordan.terrazos@gmail.com>
2021-11-04 22:09:05 -0500

    update

--- 02-Retos/07s-javascript/script/main.js
+++ 02-Retos/07s-javascript/script/main.js
@@ -1,27 +1,30 @@
-let x = parseInt(prompt("Ingrese el primer valor"));
-let y = parseInt(prompt("Ingrese el segundo valor"));
-maths(x,y);
+(function(){
+  let x = parseInt(prompt("Ingrese el primer valor"));
+  let y = parseInt(prompt("Ingrese el segundo valor"));
+  maths(x,y);
 
-function maths(alpha,omega){
-  suma(alpha,omega);
-  resta(alpha,omega);
-  multiplicacion(alpha,omega);
-  division(alpha,omega);
-  potencia(alpha,omega);
+  function maths(alpha,omega){
+    suma(alpha,omega);
+    resta(alpha,omega);
+    multiplicacion(alpha,omega);
+    division(alpha,omega);
+    potencia(alpha,omega);
 
-  function suma(a,b){
-    console.log("La suma es: ",a+b);
+    function suma(a,b){
+      console.log("La suma es: ",a+b);
+    }
+    function resta(a,b){
+      console.log("La resta es: ",a-b);
+    }
+    function multiplicacion(a,b){
+      console.log("La multiplicación es: ",a*b);
+    }
+    function division(a,b){
+      console.log("La división es: ",a/b);
+    }
+    function potencia(a,b){
+      console.log("La potencia es: ",a**b);
+    }
   }
-  function resta(a,b){
-    console.log("La resta es: ",a-b);
-  }
-  function multiplicacion(a,b){
-    console.log("La multiplicación es: ",a*b);
-  }
-  function division(a,b){
-    console.log("La división es: ",a/b);
-  }
-  function potencia(a,b){
-    console.log("La potencia es: ",a**b);
-  }
-}
+})()
+
