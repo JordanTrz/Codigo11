cc8f75f2e05ab5471439900de63d4c9bd61cc4c2
Jordan <jordan.terrazos@gmail.com>
2021-11-12 23:33:41 -0500

    update

---
+++ 02-Retos/09s-credit-card/js/main.js
@@ -0,0 +1,9 @@
+let form_cardNumber = document.querySelector('.form-cardNumber');
+let boxNumberIn = document.querySelector('.box-numberIn');
+let cardNumber_1 = document.querySelector('.cardNumber_1');
+let cardNumber_2 = document.querySelector('.cardNumber_2');
+let cardNumber_3 = document.querySelector('.cardNumber_3');
+let cardNumber_4 = document.querySelector('.cardNumber_4');
+
+let h2_1 = document.createElement('h2');
+h2_1.cloneNode(cardNumber_1);
